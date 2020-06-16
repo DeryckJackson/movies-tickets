b1763c00e4b2ea2dd2acd2fc2fbba1da522a890d
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-16 10:49:56 -0700

    add UI logic and Business logic, add html content
    Co-authored-by: JohnNils Olson <johnnils@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,51 @@
+//Business logic
+function Ticket(name, firstRelease, age, matinee, showtime) {
+  this.name = name;
+  this.firstRelease = firstRelease;
+  this.age = age;
+  this.matinee = matinee;
+  this.showtime = showtime;
+}
+
+function ticketPrice(ticket) {
+  let cost = 10;
+  if (ticket.firstRelease === true) {
+    cost += 5;
+  }
+
+  if (ticket.matinee) {
+    cost += 5;
+  } else {
+    cost += 10; 
+  }
+
+  if (ticket.age === "senior") {
+    return (cost * 0.80);
+  } else if (ticket.age === "child") {
+    return (cost * 0.90);
+  } else {
+    return cost;
+  }
+}
+
+
 $(document).ready(function() {
-  
+  $("form#input").submit(function() {
+    event.preventDefault();
+
+    const name = $("select#movie").val();
+    const firstRelease = $("select#movie option:selected").data('value');
+    const age = $("select#age").val();
+    const matinee = $("select#showtimes option:selected").data('value');
+    const showtime = $("select#showtimes").val();
+    let ticket = new Ticket(name, firstRelease, age, matinee, showtime);
+
+    const cost = ticketPrice(ticket);
+
+    $("span#price").text(`$${cost}`);
+    $("span#time").text(ticket.showtime);
+    $("div.hidden").show();
+
+
+  })
 });
